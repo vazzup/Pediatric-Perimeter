3b52ee09b69e8f1948fd8f591e792e00e2bacd2c
Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>
2016-05-25 16:38:48 +0530

    First Commit

---
+++ src/com/lvpei/pediperi/PediPeri.java
@@ -0,0 +1,5 @@
+package com.lvpei.pediperi;
+
+public class PediPeri {
+
+}
---
+++ src/com/lvpei/pediperi/form/Form.java
@@ -0,0 +1,20 @@
+package com.lvpei.pediperi.form;
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Form extends Application{
+
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Patient Details");
+		
+		GridPane gridPane = new GridPane();
+		
+		
+	}
+
+}
